806afe0d0eda20c1daf93485302bf9c8fb2f33e6
nguyentri-art <ngtri1410@gmail.com>
2024-07-30 15:51:02 +0700

    change Info

--- src/components/Public.js
+++ src/components/Public.js
@@ -4,21 +4,21 @@ const Public = () => {
     const content = (
         <section className="public">
             <header>
-                <h1>Welcome to <span className="nowrap">Dan D. Repairs!</span></h1>
+                <h1>Welcome to <span className="nowrap">A Little Dream</span></h1>
             </header>
             <main className="public__main">
-                <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
+                <p>We Connect the dreammer </p>
                 <address className="public__addr">
-                    Dan D. Repairs<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
-                    <a href="tel:+15555555555">(555) 555-5555</a>
+                    A Little Dream<br />
+                    123 Dream Street<br />
+                    456789 Blue Sky Full Moon Univer <br />
+                    <a href="tel:+123456789">(123) 123456789</a>
                 </address>
                 <br />
-                <p>Owner: Dan Davidson</p>
+                <p>A Little Dream</p>
             </main>
             <footer>
-                <Link to="/login">Employee Login</Link>
+                <Link to="/login">Dreammer Login</Link>
             </footer>
         </section>
 
